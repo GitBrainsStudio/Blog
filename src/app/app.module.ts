eb648f3ea3124a9f0448f442f3ea6730229ed085
Developer <gitbrains@gmail.com>
2020-06-30 21:56:41 +0500

    add top-menu component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostsListComponent } from './posts-list/posts-list.component';
 import { PostDetailsComponent } from './post-details/post-details.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostsListComponent,
-    PostDetailsComponent
+    PostDetailsComponent,
+    TopMenuComponent
   ],
   imports: [
     BrowserModule,
